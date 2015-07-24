5e73c6973c78997079108b1494133ffae543f766
diaconubogdan <body@darkstrike.ro>
2015-07-24 10:15:08 +0300

    added cards detail.html

--- mysite/cards/views.py
+++ mysite/cards/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Card
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
-    lastest_card_list = Card.objects.order_by('-created_at')[:20]
-    output = ', '.join([p.name for p in lastest_card_list])
-    return HttpResponse(output)
+    lastest_cards_list = Card.objects.order_by('-created_at')[:20]
+    template = loader.get_template('cards/index.html')
+    context = RequestContext(request, {
+        'lastest_cards_list': lastest_cards_list,
+    })
+    return HttpResponse(template.render(context))
 
 
 def detail(request, card_id):
-    return HttpResponse("You're looking at card %s." % card_id)
+    card = get_object_or_404(Card, pk=card_id)
+    return render(request, 'cards/detail.html', {'card': card})
